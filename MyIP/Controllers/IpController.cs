c4598aa549d7ba7759f608b975971818c6ad1ed5
BECKLESPINAX <germanosaurio@hotmail.com>
2024-10-14 02:55:46 -0300

    Add files via upload

---
+++ MyIP/Controllers/IpController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyIP.Controllers
+{
+    public class IpController : Controller
+    {
+        public ActionResult MostrarIP()
+        {
+            string userIp = ObtenerIPUsuario();
+            ViewData["IP"]  = userIp;
+            return View();
+        }
+
+        private string ObtenerIPUsuario()
+        {
+        string ip = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+    if (string.IsNullOrEmpty(ip))
+    {
+        ip = Request.ServerVariables["REMOTE_ADDR"];
+    }
+
+    // Comprobar si la IP es localhost
+    if (ip == "::1" || ip == "127.0.0.1")
+    {
+        ip = "Acceso local: IP no disponible";
+    }
+
+    return ip;
+        }
+    }
+}
---
+++ MyIP/Views/Ip/MostrarIP.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage" %>
+
+<!DOCTYPE html>
+
+<html>
+<head id="Head1" runat="server">
+    <title>Tu IP Pública</title>
+</head>
+<body>
+    <h2>Tu IP Pública es:</h2>
+    <p><%= ViewData["IP"] %></p>
+</body>
+</html>
